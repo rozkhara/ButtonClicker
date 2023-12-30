a9f54242b81f4ad958eba34209a159253c512851
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-12-30 18:34:42 +0900

    진공 엔진, 자전거, useless machine

---
+++ Assets/BicycleSpeedController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BicycleSpeedController : MonoBehaviour
+{
+    public float animationSpeed = 1.0f;
+    private Animator animator;
+
+    float getSpeedByLevel() {
+        // 레벨에 따른 값 가져오는 것으로 변경
+        return 2.5f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputSpeed = getSpeedByLevel();
+        float newSpeed = animationSpeed * inputSpeed;
+
+        animator.SetFloat("AnimSpeed", newSpeed);
+    }
+}
