587cbd3f37427ef1e51aa5d07480eebd11f51e59
milk--ways <joming1418@gmail.com>
2024-01-08 19:27:04 +0900

    카메라 이동(속도함수 확인 필요)

--- Assets/TouchManager.cs
+++ Assets/TouchManager.cs
@@ -20,12 +20,36 @@ public class TouchManager : MonoBehaviour
                 {
                     Debug.Log(hit.transform.gameObject.name);
 
-                    Camera.main.transform.position = hit.transform.position - 5f * Vector3.forward;
+                    //Camera.main.transform.position = hit.transform.position - 5f * Vector3.forward;
+
+                    StartCoroutine(LerpCamera(Camera.main.transform.position, hit.transform.position - 5f * Vector3.forward, hit.transform.gameObject.name));
 
                     //infoPanel.SetActive(true, hit.transform.gameObject.name);
-                    infoPanel.TurnOn(hit.transform.gameObject.name);
+                    //infoPanel.TurnOn(hit.transform.gameObject.name);
                 }
             }
         }
+    }
+
+    public IEnumerator LerpCamera(Vector3 startPos, Vector3 endPos,string str)
+    {
+        float delta = 0;
+        float duration = 2f;
+
+        while (delta <= duration)
+        {
+            float t = delta / duration;
+            //t = 0.25f - Mathf.Pow(t-0.5f, 2);
+            t = Mathf.Sqrt(1 - Mathf.Pow(t - 1, 4));
+            //t = t < 0.5f ? t * t : 1 - (1 - t) * (1 - t);
+
+            Camera.main.transform.position = Vector3.Lerp(startPos, endPos, t);
+
+            delta += Time.deltaTime;
+            yield return null;
+        }
+
+        Camera.main.transform.position = endPos;
+        infoPanel.TurnOn(str);
     }
 }
