931f317e17fbc846dc55f0e361ccb8875c3851dd
rozkhara <nytemute@gmail.com>
2024-02-10 15:07:09 +0900

    버튼 고침
    bool check가 반대가 돼야할 것 같아서 이해는 안되지만 아무튼 작동하니까 냅두겠음

--- Assets/ButtonBehavior.cs
+++ Assets/ButtonBehavior.cs
@@ -1,24 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ButtonBehavior : MonoBehaviour
 {
     private void OnMouseDown()
     {
-        SoundManager.Instance.PlaySFXSound("ButtonDown");
-        gameObject.transform.Translate(new Vector3(0f, -0.5f, 0f) * transform.lossyScale.x);
-        GameManager.Instance.SetScore(1);
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            SoundManager.Instance.PlaySFXSound("ButtonDown");
+            gameObject.transform.Translate(new Vector3(0f, -0.5f, 0f) * transform.lossyScale.x);
+            GameManager.Instance.SetScore(1);
+        }
     }
 
     private void OnMouseUp()
     {
-        SoundManager.Instance.PlaySFXSound("ButtonUp");
-        gameObject.transform.Translate(new Vector3(0f, 0.5f, 0f) * transform.lossyScale.x);
-    }
-
-    private void OnMouseUpAsButton()
-    {
-        
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            SoundManager.Instance.PlaySFXSound("ButtonUp");
+            gameObject.transform.Translate(new Vector3(0f, 0.5f, 0f) * transform.lossyScale.x);
+        }
     }
 }
