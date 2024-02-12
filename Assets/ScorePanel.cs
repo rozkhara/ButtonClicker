f3573befb848ad5c3da628274fcb65be5cdfae72
rozkhara <nytemute@gmail.com>
2024-02-12 21:28:05 +0900

    UI 연동

--- Assets/ScorePanel.cs
+++ Assets/ScorePanel.cs
@@ -1,34 +1,37 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class ScorePanel : MonoBehaviour
-{   
+{
 
     public TMP_Text Factory_name_set_panel;// text에 표시될 공장 이름
-    public TMP_Text User_name_set_panel ;  // text에 표시될 유저 이름
+    public TMP_Text User_name_set_panel;  // text에 표시될 유저 이름
     public TMP_Text Scoretxt;
 
-
-    public TMP_InputField Factory_name_input ; 
-    public TMP_InputField User_name_input ;
+    public TMP_InputField Factory_name_input;
+    public TMP_InputField User_name_input;
 
     public TMP_Text Factory_name_score_panel;
     public TMP_Text User_name_score_panel;
 
     private void Start()
     {
-        Factory_name_set_panel.text = "Button Factory";
-        User_name_set_panel.text = "User"; 
+        //Factory_name_set_panel.text = "Button Factory";
+        Factory_name_set_panel.text = GameManager.Instance.factoryName;
+        User_name_set_panel.text = GameManager.Instance.userName;
+        Factory_name_score_panel.text= GameManager.Instance.factoryName;
+        User_name_score_panel.text= GameManager.Instance.userName;
     }
 
     public void Edit_Factory_name_end_edit()
     {
+        GameManager.Instance.factoryName = Factory_name_input.text;
         Factory_name_set_panel.text = Factory_name_input.text;
         Factory_name_score_panel.text = Factory_name_input.text;
     }
     public void Edit_User_name_end_edit()
     {
+        GameManager.Instance.userName = User_name_input.text;
         User_name_set_panel.text = User_name_input.text;
         User_name_score_panel.text = User_name_input.text;
     }
@@ -36,8 +39,5 @@ public class ScorePanel : MonoBehaviour
     {
         Scoretxt.text = GameManager.Score.ToString();
     }
-
-
-
 
 }
