49676d539cc402c49083b9676a6b3c334af12687
milk--ways <joming1418@gmail.com>
2024-02-14 19:22:13 +0900

    Start씬 quit버튼

---
+++ Assets/QuitButtonBehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class QuitButtonBehavior : MonoBehaviour
+{
+    private Vector3 initPos;
+    private Vector3 pressedPos;
+    private bool isDown = false;
+
+    private void Awake()
+    {
+        initPos = gameObject.transform.position;
+        pressedPos = gameObject.transform.position + new Vector3(0f, -0.5f, 0f) * transform.lossyScale.x;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            SoundManager.Instance.PlaySFXSound("ButtonDown");
+            this.gameObject.transform.position = pressedPos;
+            isDown = true;
+            Application.Quit();
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (isDown)
+        {
+            SoundManager.Instance.PlaySFXSound("ButtonUp");
+            this.gameObject.transform.position = initPos;
+            isDown = false; 
+        }
+    }
+}
