cebc114d4162c4b1bea6ff04a8572b691bf69dea
wjddbs1509 <wjddbs1509@gmail.com>
2024-02-15 22:32:34 +0900

    ui 추가 및 코드 연결

--- Assets/PanelPrefab.cs
+++ Assets/PanelPrefab.cs
@@ -14,28 +14,35 @@ public class PanelPrefab : MonoBehaviour
     public int automata_id;
     string assetRoute;
     Automata automata;
-
-
+    int quantity;
+    string autoname;
 
     public void SetPanelPrefab(int index) 
     {
         automata_id = index;
+        autoname = GameManager.Instance.prefabs[automata_id].GetComponent<Automata>().automata_data.name;
+        quantity = GameManager.Instance.prefabs[automata_id].GetComponent<Automata>().quantity;
         assetRoute = "Image/" + GameManager.Instance.prefabs[automata_id].GetComponent<Automata>().automata_data.name;
         transform.GetChild(0).GetComponent<RawImage>().texture = Resources.Load<Texture>(assetRoute);
-        transform.GetChild(1).GetComponent<TMP_Text>().text = GameManager.Instance.prefabs[automata_id].GetComponent<Automata>().automata_data.name;
+        transform.GetChild(1).GetComponent<TMP_Text>().text = autoname + " " + "Lv." + quantity.ToString();
         transform.GetChild(2).GetComponent<TMP_Text>().text = "Now:" + DisplayNumber.EnglishNumber(GameManager.Instance.prefabs[automata_id].GetComponent<Automata>().all_production);
+        transform.GetChild(3).GetComponent<TMP_Text>().text = "Next:" + DisplayNumber.EnglishNumber(Balancing.Produce[automata_id, quantity]);
         automata = GameManager.Instance.prefabs[automata_id].GetComponent<Automata>();
     }
 
     void Update()
     {
-        if (GameManager.Score >= transform.GetChild(4).GetComponent<BuyButtonUITen>().price10)
+        quantity = GameManager.Instance.prefabs[automata_id].GetComponent<Automata>().quantity;
+        transform.GetChild(1).GetComponent<TMP_Text>().text = autoname + " " + "Lv." + quantity.ToString();
+        transform.GetChild(2).GetComponent<TMP_Text>().text = "Now:" + DisplayNumber.EnglishNumber(GameManager.Instance.prefabs[automata_id].GetComponent<Automata>().all_production);
+        transform.GetChild(3).GetComponent<TMP_Text>().text = "Next:" + DisplayNumber.EnglishNumber(Balancing.Produce[automata_id, quantity]);
+        if (GameManager.Score >= transform.GetChild(5).GetComponent<BuyButtonUITen>().price10)
         {
-            transform.GetChild(4).gameObject.SetActive(true);
+            transform.GetChild(5).gameObject.SetActive(true);
         }
         else
         {
-            transform.GetChild(4).gameObject.SetActive(false);
+            transform.GetChild(5).gameObject.SetActive(false);
         }
     }
 
